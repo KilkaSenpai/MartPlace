25ec75c7cabf2839108d2fe5870a459d009c63a7
KilkaSenpai <grekagrove@gmail.com>
2021-02-27 23:20:37 +0200

    added first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,36 @@
 $(function () {
   var mixer = mixitup('.products__items');
   $(".create__select").select2({
-    selectionCssClass:  'create__select',
+    selectionCssClass: 'create__select',
     dropdownCssClass: 'create__option'
   });
 
-  new Swiper('.swiper-container', {
+  new Swiper('.resume__container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     loop: true,
   });
-  var options = {
-    initial_value: 4.5,
-    readonly: true,
-  }
+  new Swiper('.followers__container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+  });
+  new Swiper('.feedback__container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+  });
+
   $(".product-card__star").rate(options);
   var options = {
     initial_value: 4.5,
